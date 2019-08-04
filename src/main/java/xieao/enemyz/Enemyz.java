d9ba4ab330e52cc4f86730f6eca975890b57efde
xieaoDev <12222738+xieaoDev@users.noreply.github.com>
2019-08-04 21:29:03 +0100

    Fix crash caused by mappings.

--- src/main/java/xieao/enemyz/Enemyz.java
+++ src/main/java/xieao/enemyz/Enemyz.java
@@ -3,22 +3,28 @@ package xieao.enemyz;
 import com.mojang.blaze3d.platform.GLX;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
@@ -62,15 +68,12 @@ public class Enemyz {
     public static class Config {
         public final ForgeConfigSpec.DoubleValue iconSize;
         public final ForgeConfigSpec.DoubleValue yOffset;
-        public final ForgeConfigSpec.BooleanValue sneaking;
 
         public Config(ForgeConfigSpec.Builder builder) {
             this.iconSize = builder.comment("Change the icon size.")
                     .defineInRange("icon_size", 1.0D, 0.0D, 1.0D);
             this.yOffset = builder.comment("Move the icon up and down.")
                     .defineInRange("icon_y_offset", 0.0D, -1.0D, 1.0D);
-            this.sneaking = builder.comment("Show icon through blocks while sneaking.")
-                    .define("sneaking", true);
         }
     }
 
@@ -103,6 +106,16 @@ public class Enemyz {
         }
     }
 
+    public static final Item ICON = new Item(new Item.Properties());
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class Handler2 {
+        @SubscribeEvent
+        public static void onRegistry(RegistryEvent.Register<Item> event) {
+            event.getRegistry().register(ICON.setRegistryName("icon"));
+        }
+    }
+
     @Mod.EventBusSubscriber
     public static class Handler {
         private static final UUID EMPTY_UUID = new UUID(0L, 0L);
@@ -110,7 +123,7 @@ public class Enemyz {
         @SubscribeEvent
         public static void setAndSyncTarget(LivingEvent.LivingUpdateEvent event) {
             LivingEntity entity = event.getEntityLiving();
-            World world = entity.world;
+            World world = entity.getEntityWorld();
             if (!world.isRemote) {
                 if (entity instanceof MobEntity) {
                     MobEntity mobEntity = (MobEntity) entity;
@@ -135,8 +148,6 @@ public class Enemyz {
             }
         }
 
-        public static final ResourceLocation LOCATION = new ResourceLocation(MOD_ID, "textures/misc/skull.png");
-
         @SubscribeEvent
         @OnlyIn(Dist.CLIENT)
         public static void renderIcon(RenderLivingEvent.Post event) {
@@ -145,35 +156,27 @@ public class Enemyz {
             UUID uuid = entity.getEntityData().getUniqueId(TAG_PLAYER_UUID);
             if (player.getUniqueID().equals(uuid) && entity.isAlive()) {
                 GlStateManager.pushMatrix();
-                Minecraft.getInstance().getTextureManager().bindTexture(LOCATION);
                 float neatOffset = ModList.get().isLoaded("neat") ? 0.35F : 0.0F;
                 GlStateManager.translated(event.getX(), entity.getHeight() + 0.5D + event.getY() + CONFIG.yOffset.get() + neatOffset, event.getZ());
-                GlStateManager.enableRescaleNormal();
                 GlStateManager.enableBlend();
-                GlStateManager.depthMask(false);
-                boolean isSneaking = player.isSneaking();
-                if (CONFIG.sneaking.get() && isSneaking) {
-                    GlStateManager.disableDepthTest();
-                }
                 GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE);
                 GLX.glMultiTexCoord2f(GLX.GL_TEXTURE1, 15728880 >> 16 & '\uffff', 15728880 & '\uffff');
                 GlStateManager.scalef((float) (0.5F * CONFIG.iconSize.get()), (float) (0.5F * CONFIG.iconSize.get()), (float) (0.5F * CONFIG.iconSize.get()));
                 GlStateManager.color4f(1.0F, 0.0F, 0.0F, 1.0F);
-                Tessellator tessellator = Tessellator.getInstance();
-                BufferBuilder bufferbuilder = tessellator.getBuffer();
                 GlStateManager.rotatef(180.0F - event.getRenderer().getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-                bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_NORMAL);
-                bufferbuilder.pos(-0.5D, -0.25D, 0.0D).tex(0.0D, 1.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
-                bufferbuilder.pos(0.5D, -0.25D, 0.0D).tex(1.0D, 1.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
-                bufferbuilder.pos(0.5D, 0.75D, 0.0D).tex(1.0D, 0.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
-                bufferbuilder.pos(-0.5D, 0.75D, 0.0D).tex(0.0D, 0.0D).normal(0.0F, 1.0F, 0.0F).endVertex();
-                tessellator.draw();
-                if (CONFIG.sneaking.get() && isSneaking) {
-                    GlStateManager.enableDepthTest();
-                }
-                GlStateManager.depthMask(true);
+                ItemStack stack = new ItemStack(ICON);
+                ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+                TextureManager textureManager = Minecraft.getInstance().getTextureManager();
+                textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+                textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
+                IBakedModel bakedmodel = ForgeHooksClient.handleCameraTransforms(itemRenderer.getModelWithOverrides(stack), ItemCameraTransforms.TransformType.FIXED, false);
+                GlStateManager.disableLighting();
+                GlStateManager.scalef(1.0F, 1.0F, 0.3F);
+                itemRenderer.renderItem(stack, bakedmodel);
+                GlStateManager.enableLighting();
+                textureManager.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+                textureManager.getTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
                 GlStateManager.disableBlend();
-                GlStateManager.disableRescaleNormal();
                 GlStateManager.popMatrix();
             }
         }
